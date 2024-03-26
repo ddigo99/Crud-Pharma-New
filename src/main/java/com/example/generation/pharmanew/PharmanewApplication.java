a52f9478da156d11e1c7e7db25af6a2f39c626db
Rodrigo Rodrigues <rodrigonrce99@gmail.com>
2024-03-26 10:06:49 -0300

    Primeira versão, configurando pom.xml e application.properties do projeto

---
+++ src/main/java/com/example/generation/pharmanew/PharmanewApplication.java
@@ -0,0 +1,13 @@
+package com.example.generation.pharmanew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmanewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmanewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/generation/pharmanew/PharmanewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.generation.pharmanew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmanewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
