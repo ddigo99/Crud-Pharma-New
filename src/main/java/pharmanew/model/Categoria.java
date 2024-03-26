f53f15abf3cb25f3638b3fc1d02b6d72b6e013ac
Rodrigo Rodrigues <rodrigonrce99@gmail.com>
2024-03-26 13:17:55 -0300

    Terceira versão, adicionando entidade produto e criando relacionamentos

--- src/main/java/pharmanew/model/Categoria.java
+++ src/main/java/pharmanew/model/Categoria.java
@@ -1,9 +1,16 @@
 package pharmanew.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -12,17 +19,29 @@ import jakarta.validation.constraints.Size;
 @Table(name = "tb_categorias")
 public class Categoria {
 
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotBlank(message = "O atributo nome é Obrigatório")
-	@Size(min = 5, max = 110, message = "O atributo nome categoria deve conter no mínimo 5 e no máximo 110 caracteres")
+	@NotBlank(message = "O atributo nome categoria é Obrigatório")
+	@Size(min = 5, max = 110, message = "O atributo nome categoria deve conter no mínimo 5 e no máximo 255 caracteres")
 	private String nome;
 
-	@NotBlank(message = "O atributo descrição é Obrigatório")
-	@Size(min = 5, max = 255, message = "O atributo descrição deve conter no mínimo 5 e no máximo 255 caracteres")
+	@NotBlank(message = "O atributo descricao é Obrigatório")
+	@Size(min = 5, max = 255, message = "O atributo descricao deve conter no mínimo 5 e no máximo 255 caracteres")
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
