f6ac41ceadc4bab6271d94ceb117a2c127db28c7
Rodrigo Rodrigues <rodrigonrce99@gmail.com>
2024-03-26 11:26:52 -0300

    Segunda versão, adicionando crud de categoria e realizando ajuste no application.properties

---
+++ src/main/java/pharmanew/model/Categoria.java
@@ -0,0 +1,51 @@
+package pharmanew.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo nome é Obrigatório")
+	@Size(min = 5, max = 110, message = "O atributo nome categoria deve conter no mínimo 5 e no máximo 110 caracteres")
+	private String nome;
+
+	@NotBlank(message = "O atributo descrição é Obrigatório")
+	@Size(min = 5, max = 255, message = "O atributo descrição deve conter no mínimo 5 e no máximo 255 caracteres")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getdescricao() {
+		return descricao;
+	}
+
+	public void setdescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
---
+++ src/main/java/pharmanew/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package pharmanew.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pharmanew.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+}
